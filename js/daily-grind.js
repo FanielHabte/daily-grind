66f29b284a6fb5d3ab0531ccf084528ec8ffa5f0
FanielHabte <fanielhabte20@gmail.com>
2023-06-06 01:15:50 -0700

    update

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -12,35 +12,51 @@
 
 
 
+  function coffeeTemplate(coffee) {
+    return `
+      <p>
+        <img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
+        <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is
+        <strong class="feature">${coffee.name}</strong>, ${coffee.desc}
+      </p>
+    `;
+  }
 
 
 let myDate = new Date();
 let today = myDate.getDay();
-today = 3;
-switch (today){
-   case 2:
-    today= "Tuesday";
-   break;
 
-   case 3:
-    today= "Wednesday";
-   break;
+//today = 1;
+  switch (today) {
+    case 1:
+      today = "Monday";
+      coffee = {
+        name: "Bubble Tea",
+        color: "pink",
+        pic: "images/bubble-tea.jpg",
+        alt: "A picture of a bubble tea",
+        day: "Monday",
+        desc: "I like me some bubble tea!",
+      };
+  break;
+
+  case 2:
+    today= "Tuesday";
+  break;
+
+  case 3:
+    today= "Wendesday";
+  break;
 
    default:
-today = "Not sure what day it is";
+    today = "Not sure what day it is";
 }
 
 
 
-coffee = {
-    name: "Bubble Tea",
-    color: "pink",
-    pic:"images/bubble-tea.jpg",
-    alt:"A picture of a bubble tea",
-    Day:"Wednesday",
-    desc:`I like me some bubble tea!`
-}
 
+
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
 
 //alert(today);
 console.log(coffee);
