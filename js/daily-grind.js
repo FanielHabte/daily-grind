e5b92a256d10b8ed328802445a4910f718d4e40d
FanielHabte <fanielhabte20@gmail.com>
2023-06-08 02:09:46 -0700

    update

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -24,9 +24,25 @@
 
 
 let myDate = new Date();
+let coffee = "";
 let today = myDate.getDay();
 
-//today = 1;
+
+//use location object to access querystring (address bar)
+const queryString = window.location.search;
+    
+//output to console    
+console.log(queryString);
+    
+//separate query string parameters
+const urlParams = new URLSearchParams(queryString);
+
+//today = 3;
+
+if(urlParams.has("day")){//from querystring
+  today = urlParams.get("day");}
+ today= parseInt(today); // changes the string  to number
+// swith function to change from day to day
   switch (today) {
     case 1:
       today = "Monday";
@@ -42,21 +58,77 @@ let today = myDate.getDay();
 
   case 2:
     today= "Tuesday";
+    coffee = {
+      name: "Drip",
+      color: "black",
+      pic: "images/drip.jpg",
+      alt: "A picture of a Drip Coffee",
+      day: "Tuesday",
+      desc: " give it to me straight!",
+    };
   break;
 
   case 3:
     today= "Wendesday";
+    coffee = {
+      name: "Caramel Latte",
+      color: "beige",
+      pic: "images/caramel-latte.jpg",
+      alt: "A picture of caramel latte",
+      day: "Wendesday",
+      desc: " Start your day with Caramel Latte!",
+    };
   break;
 
-   default:
-    today = "Not sure what day it is";
+  case 4:
+    today= "Thursday";
+    coffee = {
+      name: "Cold Brew",
+      color: "#590000",
+      pic: "images/cold-brew.jpg",
+      alt: "A picture of Cold Brew",
+      day: "Thursday",
+      desc: " Enjoy your summer with our chillied Cold Brew!",
+    };
+  break;
+
+  case 5:
+    today= "Friday";
+    coffee = {
+      name: "Frappaccino",
+      color: "#8B4513",
+      pic: "images/frappaccino.jpg",
+      alt: "A picture of Frappaccino",
+      day: "Friday",
+      desc: " Enjoy our Chocolate topped Frap!",
+    };
+  break;
+
+  case 6:
+    today= "Saturday";
+    coffee = {
+      name: "Mocha",
+      color: "#FFF5EE",
+      pic: "images/mocha.jpg",
+      alt: "A picture of Mocha",
+      day: "Saturday",
+      desc: " Enjoy your day with our italian style Mocha!",
+    };
+  break;
+  
+  default:
+    today= "Sunday";
+    coffee = {
+      name: "Pumpkin Spice",
+      color: "#FF7518",
+      pic: "images/pumpkin-spice-latte.jpg",
+      alt: "A Our Pumpkin Spice Latte tastes great on a Fall Day! of Pumkin Spice Latte",
+      day: "Sunday",
+      desc: "give which makes us wish it was always Fall, as this is one of our top sellers! to me straight!",
+    };
 }
 
-
-
-
-
 document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
-
+document.querySelector("html").style.backgroundColor= coffee.color;
 //alert(today);
 console.log(coffee);
